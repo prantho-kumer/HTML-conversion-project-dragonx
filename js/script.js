f6e9c22772d5cc45d18d0d565a0452647a45ca1b
Prantho Kumer <57418052+prantho-kumer@users.noreply.github.com>
2022-11-16 09:08:31 +0600

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -7,40 +7,173 @@ $(document).ready(function () {
     adaptiveHeight: false,
   });
 
-  // review slider
- $(".reviews-slider").slick({
-   dots: true,
-   infinite: true,
-   speed: 300,
-   slidesToShow: 3,
-   slidesToScroll: 1,
-   responsive: [
-     {
-       breakpoint: 1024,
-       settings: {
-         slidesToShow: 2,
-         slidesToScroll: 1,
-         infinite: true,
-         dots: true,
-       },
-     },
-     {
-       breakpoint: 767,
-       settings: {
-         slidesToShow: 2,
-         slidesToScroll: 1,
-       },
-     },
-     {
-       breakpoint: 480,
-       settings: {
-         slidesToShow: 1,
-         slidesToScroll: 1,
-       },
-     },
-   ],
- });
-				
+  // review slider gold
+  $(".reviews-slider").slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 767,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  });
+
+  $("#nav-drag_silver-tab").click(function () {
+    $(".pro-img-slider_silver").slick({
+      infinite: true,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+    });
+    // review slider
+    $(".reviews-slider_silver").slick({
+      dots: true,
+      infinite: true,
+      speed: 300,
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 1,
+            infinite: true,
+            dots: true,
+          },
+        },
+        {
+          breakpoint: 767,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 1,
+          },
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+          },
+        },
+      ],
+    });
+  });
+
+  // product single gallery
+  // product slider
+  $(".pro-single-gallery").slick({
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    adaptiveHeight: false,
+  });
+
+
+// quantity +-
+  $(".btn-number").click(function (e) {
+    e.preventDefault();
+
+    fieldName = $(this).attr("data-field");
+    type = $(this).attr("data-type");
+    var input = $("input[name='" + fieldName + "']");
+    var currentVal = parseInt(input.val());
+    if (!isNaN(currentVal)) {
+      if (type == "minus") {
+        if (currentVal > input.attr("min")) {
+          input.val(currentVal - 1).change();
+        }
+        if (parseInt(input.val()) == input.attr("min")) {
+          $(this).attr("disabled", true);
+        }
+      } else if (type == "plus") {
+        if (currentVal < input.attr("max")) {
+          input.val(currentVal + 1).change();
+        }
+        if (parseInt(input.val()) == input.attr("max")) {
+          $(this).attr("disabled", true);
+        }
+      }
+    } else {
+      input.val(0);
+    }
+  });
+  $(".input-number").focusin(function () {
+    $(this).data("oldValue", $(this).val());
+  });
+  $(".input-number").change(function () {
+    minValue = parseInt($(this).attr("min"));
+    maxValue = parseInt($(this).attr("max"));
+    valueCurrent = parseInt($(this).val());
+
+    name = $(this).attr("name");
+    if (valueCurrent >= minValue) {
+      $(".btn-number[data-type='minus'][data-field='" + name + "']").removeAttr("disabled");
+    } else {
+      alert("Sorry, the minimum value was reached");
+      $(this).val($(this).data("oldValue"));
+    }
+    if (valueCurrent <= maxValue) {
+      $(".btn-number[data-type='plus'][data-field='" + name + "']").removeAttr("disabled");
+    } else {
+      alert("Sorry, the maximum value was reached");
+      $(this).val($(this).data("oldValue"));
+    }
+  });
+  $(".input-number").keydown(function (e) {
+    // Allow: backspace, delete, tab, escape, enter and .
+    if (
+      $.inArray(e.keyCode, [46, 8, 9, 27, 13, 190]) !== -1 ||
+      // Allow: Ctrl+A
+      (e.keyCode == 65 && e.ctrlKey === true) ||
+      // Allow: home, end, left, right
+      (e.keyCode >= 35 && e.keyCode <= 39)
+    ) {
+      // let it happen, don't do anything
+      return;
+    }
+    // Ensure that it is a number and stop the keypress
+    if ((e.shiftKey || e.keyCode < 48 || e.keyCode > 57) && (e.keyCode < 96 || e.keyCode > 105)) {
+      e.preventDefault();
+    }
+  });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 });
+
